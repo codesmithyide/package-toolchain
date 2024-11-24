9161a32f63e12137cdcc5e52c504ec767557e45f
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2024-11-24 23:55:53 +0000

    Updates

---
+++ include/CodeSmithy/PackageToolchains.hpp
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2020-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#ifndef GUARD_CODESMITHYIDE_PACKAGETOOLCHAINS_HPP
+#define GUARD_CODESMITHYIDE_PACKAGETOOLCHAINS_HPP
+
+#include "PackageToolchains/linkoptions.hpp"
+
+#endif
