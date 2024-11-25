cd25d6ebf817bdc3ac456639f1df1a5244b690eb
Xavier Leclercq <xavier@leclercq.dev>
2024-11-25 22:59:51 +0000

    Add test suite (#3)

---
+++ tests/src/main.cpp
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2020-2024 Xavier Leclercq
+// SPDX-License-Identifier: MIT
+
+#include "DebianPackageToolchainTests.hpp"
+#include "CodeSmithy/PackageToolchains/linkoptions.hpp"
+#include <Ishiko/TestFramework.hpp>
+#include <exception>
+
+using namespace Ishiko;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        TestHarness::CommandLineSpecification command_line_spec;
+        Configuration configuration = command_line_spec.createDefaultConfiguration();
+        CommandLineParser::parse(command_line_spec, argc, argv, configuration);
+
+        TestHarness the_test_harness("CodeSmithyPackageToolchains Library Tests", configuration);
+
+        TestSequence& the_tests = the_test_harness.tests();
+        the_tests.append<DebianPackageToolchainTests>();
+
+        return the_test_harness.run();
+    }
+    catch (const std::exception& e)
+    {
+        return TestApplicationReturnCode::exception;
+    }
+    catch (...)
+    {
+        return TestApplicationReturnCode::exception;
+    }
+}
